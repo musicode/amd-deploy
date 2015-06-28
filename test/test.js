8f7ec4cbfee35b45aa756efc0b0071cf1e292c7e
musicode <zhujialu@outlook.com>
2015-06-28 20:39:22 +0800

    update

---
+++ test/test.js
@@ -0,0 +1,55 @@
+
+var amdDeploy = require('../index');
+
+amdDeploy({
+    path: '/Users/zhujl/github/www-fe/src/pay/course.js',
+    callback: function (code) {
+        console.log(code);
+    },
+    config: {
+        baseUrl: '/Users/zhujl/github/www-fe/src',
+        paths: {
+            cobble: '../dep/cobble/0.3.19/src/',
+            ab: './common'
+        },
+        combine: {
+            'ab/service': 1,
+            'pay/course': 1
+        },
+        replace: function (requireId, resourcePath) {
+            return requireId;
+        },
+        packages: [
+            {
+                "name": "cobble",
+                "location": "../dep/cobble/0.3.19/src/",
+                "main": "main"
+            },
+            {
+                "name": "moment",
+                "location": "../dep/moment/2.7.0/src",
+                "main": "moment"
+            },
+            {
+                "name": "imageCrop",
+                "location": "../dep/imageCrop/0.0.1/src",
+                "main": "imageCrop"
+            },
+            {
+                "name": "underscore",
+                "location": "../dep/underscore/1.6.0/src",
+                "main": "underscore"
+            },
+            {
+                "name": "audioPlayer",
+                "location": "../dep/audioPlayer/0.0.1/src",
+                "main": "audioPlayer"
+            },
+            {
+                "name": "TextClipboard",
+                "location": "../dep/TextClipboard/0.0.2/src",
+                "main": "TextClipboard"
+            }
+        ]
+    }
+});
